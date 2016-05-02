32ac55ecffe04d9c7374b2d2b51de4b1c3a58834
Viktor Pikaev <HaruAtari@gmail.com>
2016-05-02 22:37:03 +0300

    Post 17: added comments.

--- 17/Programm.cs
+++ 17/Programm.cs
@@ -18,12 +18,21 @@ namespace ConsoleApplication
         public string Value { get; }
         Graph owner;
 
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="value">Value.</param>
+        /// <param name="owner">Owning graph.</param>
         public Node(string value, Graph owner)
         {
             Value = value;
             this.owner = owner;
         }
 
+        /// <summary>
+        /// Return list of nodes connected with this node.
+        /// </summary>
+        /// <returns>List of connected nodes.</returns>
         public Dictionary<Node, Weight> GetConnectedNodes()
         {
             return owner.GetConnetedNodes(this);
@@ -33,7 +42,13 @@ namespace ConsoleApplication
     class Graph
     {
         Dictionary<Node, Dictionary<Node, Weight>> map = new Dictionary<Node, Dictionary<Node, Weight>>();
+        Weight defaultWeight = 1;
 
+        /// <summary>
+        /// Add to graph new node with the specified value.
+        /// </summary>
+        /// <param name="value">Added value.</param>
+        /// <returns>Created node.</returns>
         public Node CreateNode(string value)
         {
             var node = new Node(value, this);
@@ -41,20 +56,38 @@ namespace ConsoleApplication
             return node;
         }
 
-        public void AddEdge(Node n1, Node n2, Weight weight)
+        /// <summary>
+        /// Add edge between two nodes.
+        /// </summary>
+        /// <param name="n1">Begin node.</param>
+        /// <param name="n2">End node.</param>
+        /// <param name="bidirect">Create bidirect edge.</param>
+        public void AddEdge(Node n1, Node n2, bool bidirect)
         {
-            map[n1].Add(n2, weight);
+            AddEdge(n1, n2, bidirect, defaultWeight);
         }
 
-        public void AddEdge(Node n1, Node n2, Weight weight, bool bidirect)
+        /// <summary>
+        /// Add edge between two nodes.
+        /// </summary>
+        /// <param name="n1">Begin node.</param>
+        /// <param name="n2">End node.</param>
+        /// <param name="bidirect">Create bidirect edge.</param>
+        /// <param name="weight">Weight of edge.</param>
+        public void AddEdge(Node n1, Node n2, bool bidirect, Weight weight)
         {
-            AddEdge(n1, n2, weight);
+            map[n1].Add(n2, weight);
             if (bidirect)
             {
-                AddEdge(n2, n1, weight);
+                map[n2].Add(n1, weight);
             }
         }
 
+        /// <summary>
+        /// Return list of nodes connected with specified node.
+        /// </summary>
+        /// <param name="node">List of connected nodes.</param>
+        /// <returns></returns>
         public Dictionary<Node, Weight> GetConnetedNodes(Node node)
         {
             return map.ContainsKey(node) ? map[node] : new Dictionary<Node, int>();
