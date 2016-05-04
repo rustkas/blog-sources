71c14a7bb3f16f971a6a5b3c88262fc1a275e92e
Viktor Pikaev <HaruAtari@gmail.com>
2016-05-04 23:00:50 +0300

    Post 17: added DLS algorithm.

--- 17/DepthFirstSearch.cs
+++ 17/DepthFirstSearch.cs
@@ -42,5 +42,42 @@ namespace Graphs
             }
             return false;
         }
+
+        public LinkedList<Node> DLS(Node start, Node goal, int limit)
+        {
+            Console.WriteLine($"Search path from {start.Name} to {goal.Name}:");
+            visited = new HashSet<Node>();
+            path = new LinkedList<Node>();
+            this.goal = goal;
+            DLS(start, limit);
+            if (path.Count > 0)
+            {
+                path.AddFirst(start);
+            }
+            return path;
+        }
+
+        private bool DLS(Node node, int limit)
+        {
+            node.Handler();
+            if (node == goal)
+            {
+                return true;
+            }
+            if (limit == 0)
+            {
+                return false;
+            }
+            visited.Add(node);
+            foreach (var child in node.Children.Where(x => !visited.Contains(x)))
+            {
+                if (DLS(child, limit-1))
+                {
+                    path.AddFirst(child);
+                    return true;
+                }
+            }
+            return false;
+        }
     }
 }
