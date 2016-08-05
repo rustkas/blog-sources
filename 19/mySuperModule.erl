568577821ffc9ebde39a5393688d9decef94f1db
Viktor Pikaev <HaruAtari@gmail.com>
2016-08-05 16:53:32 +0300

    Added sources for posts №19 and №20

---
+++ 19/mySuperModule.erl
@@ -0,0 +1,15 @@
+-module(mySuperModule).
+-export([add/2, subtr/2, mult/2, divis/2]).
+-author("Haru Atari").
+
+add(X, Y) -> 
+    X + Y.
+
+subtr(X, Y) ->
+    X - Y.
+
+mult(X, Y) ->
+    X * Y.
+
+divis(X, Y) ->
+    X / Y.
